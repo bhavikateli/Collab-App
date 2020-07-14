b0b6c0b9f19a7b1dca9218c53441a216df565077
Bhavika Teli <bhaviteli@fb.com>
2020-07-14 19:46:39 -0400

    basic framework of bottom navigation view

--- app/src/main/java/com/bhavikateli/collab/MainActivity.java
+++ app/src/main/java/com/bhavikateli/collab/MainActivity.java
@@ -2,63 +2,54 @@ package com.bhavikateli.collab;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.bhavikateli.collab.fragments.ComposeFragment;
+import com.bhavikateli.collab.fragments.DiscoveryFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    private EditText etDescription;
-    private Button btnCaptureImage;
-    private Button btnSubmit;
-    private ImageView ivPostImage;
     private BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etDescription = findViewById(R.id.etDescription);
-        btnCaptureImage = findViewById(R.id.btnCaptureImage);
-        ivPostImage = findViewById(R.id.ivPostImage);
-        btnSubmit = findViewById(R.id.btnSubmit);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        
-        queryPosts();
 
-        //setting on click listener to bottom navigation view
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 Fragment fragment;
                 switch (menuItem.getItemId()) {
                     case R.id.action_discovery:
-                        Toast.makeText(MainActivity.this, "Inside discovery", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "discovery!", Toast.LENGTH_SHORT).show();
+                        fragment = new DiscoveryFragment();
                         break;
                     case R.id.action_compose:
-                        Toast.makeText(MainActivity.this, "Inside compose", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "compose!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
                         break;
                     case R.id.action_profile:
-                        Toast.makeText(MainActivity.this, "Inside profile", Toast.LENGTH_SHORT).show();
                     default:
+                        Toast.makeText(MainActivity.this, "profile!", Toast.LENGTH_SHORT).show();
+                        fragment = new ComposeFragment();
                         break;
                 }
-               // fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
                 return true;
             }
         });
-        
-    }
-
-    private void queryPosts() {
+        //set default action
+        bottomNavigationView.setSelectedItemId(R.id.action_discovery);
     }
 }
---
+++ app/src/main/java/com/bhavikateli/collab/fragments/DiscoveryFragment.java
@@ -0,0 +1,26 @@
+package com.bhavikateli.collab.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.bhavikateli.collab.R;
+
+
+public class DiscoveryFragment extends Fragment {
+
+
+    public DiscoveryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discovery, container, false);
+    }
+}
