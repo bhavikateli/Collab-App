b38c114195f69ef7ca413800ccea86f49c9b93cb
Bhavika Teli <bhaviteli@fb.com>
2020-07-15 21:52:37 -0400

    created custom toolbar

--- app/src/main/java/com/bhavikateli/collab/MainActivity.java
+++ app/src/main/java/com/bhavikateli/collab/MainActivity.java
@@ -1,11 +1,13 @@
 package com.bhavikateli.collab;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -19,11 +21,15 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
     private BottomNavigationView bottomNavigationView;
     final FragmentManager fragmentManager = getSupportFragmentManager();
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
@@ -52,5 +58,12 @@ public class MainActivity extends AppCompatActivity {
         });
         //set default action
         bottomNavigationView.setSelectedItemId(R.id.action_discovery);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
